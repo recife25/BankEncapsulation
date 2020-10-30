5cddf93e1c3af91008635816d7d9faa9a0e6cea5
recife25 <felipe.j.ruiz64@gmail.com>
2020-10-29 22:06:41 -0400

    added BankEncapsulation

---
+++ BankAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+
+        }
+    }
+}
