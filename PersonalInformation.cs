527d7952ea6be8ef27fd3a92ee1da39f47e1a1d9
recife25 <felipe.j.ruiz64@gmail.com>
2020-11-20 23:02:09 -0500

    added properties

---
+++ PersonalInformation.cs
@@ -0,0 +1,43 @@
+﻿//Felipe Ruiz 
+//using System is called using directive
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BankEncapsulation
+{
+    //class PersonalInformation with default constructor 
+    public class PersonalInformation
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+
+        //create a field or a var in a class
+        private string phoneNumber;
+
+        public string NumberFormatted 
+        {
+            get 
+            {
+                return phoneNumber; 
+            }
+            set 
+            {
+                //regex.replace is a method 
+                phoneNumber = Regex.Replace($"{value}", @"(\d{3})(\d{3})(\d{4})", "($1)-$2-$3");
+
+
+            }
+
+        }
+
+
+       
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿//Felipe Ruiz 
 //Encapsulation Bank app
 using System;
+using System.Collections.Generic;
 
 namespace BankEncapsulation
 {
@@ -9,12 +10,16 @@ namespace BankEncapsulation
         static void Main(string[] args)
         {
            var account = new BankAccount();
-            //BankAccount account = new BankAccount();
-            Console.WriteLine("Amount to deposit");
-            var amountToDep = double.Parse(Console.ReadLine());
-            account.Deposit(amountToDep);
-            Console.WriteLine($"Thank you Your balance is now {account.GetBalance()}");
-            
+            ////BankAccount account = new BankAccount();
+            //Console.WriteLine("Amount to deposit");
+            //var amountToDep = double.Parse(Console.ReadLine());
+            //account.Deposit(amountToDep);
+            //Console.WriteLine($"Thank you Your balance is now {account.GetBalance()}");
+            var philsInfo = new PersonalInformation();
+
+            Console.WriteLine("Enter your phone number include area code");
+            philsInfo.NumberFormatted = Console.ReadLine();
+            Console.WriteLine($"Your new phone number is {philsInfo.NumberFormatted}");
         }
     }
 }
